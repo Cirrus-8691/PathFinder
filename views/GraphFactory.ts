11aab569dca0ac564fc662ddcef2cf89d180df87
cirrus-8691 <diaz.frederic@gmail.com>
2020-11-27 09:45:13 +0100

    graph comme il devrait etre

--- views/GraphFactory.ts
+++ views/GraphFactory.ts
@@ -14,16 +14,25 @@ export default class GraphFactory {
      */
     public static Build(graph : ReadonlyArray<Vertex>) : string[]
     {
-        let vue = "";
-        for(let id=0; id<graph.length; id++) {
-            vue += id + "-" + graph[id]
-        }
-         return [ 
-                "0-4" ,
-                "| |",
-                "3-1",
-                "|",
-                "2"
-            ];
-    }
+        //let vue = "";
+        //for(let id=0; id<graph.length; id++) {
+        //    vue += id + "-" + graph[id]
+        //}
+
+        return [
+            "...............",
+            ".+---+...+---+.",
+            ".| 0 |---| 4 |.",
+            ".+---+...+---+.",
+            "...|.......|...",
+            ".+---+...+---+.",
+            ".| 3 |---| 1 |.",
+            ".+---+...+---+.",
+            "...|...........",
+            ".+---+.........",
+            ".| 2 |.........",
+            ".+---+.........",
+            "..............."
+        ];
+   }
 }
