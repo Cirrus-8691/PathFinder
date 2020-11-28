d37a2a5ec09ced36aeacf58c4cd8d9abc1693311
cirrus-8691 <diaz.frederic@gmail.com>
2020-11-28 18:19:48 +0100

    ASCII_GraphFactory

--- src/views/GraphFactory.ts
+++ src/views/GraphFactory.ts
@@ -1,3 +1,4 @@
+import { Edge } from "../domain/interfaces/Edge";
 import { Vertex } from "../domain/interfaces/Vertex";
 import { Path } from "./interfaces/Path";
 
@@ -16,24 +17,6 @@ export default class GraphFactory {
      */
     public static Build(graph : ReadonlyArray<Vertex>) : string[]
     {
-        // no so easy to do.
-        /*
-        return [
-            "...................",
-            ".+-----+ . +-----+.",
-            ".| 000 |---| 004 |.",
-            ".+-----+ . +-----+.",
-            "... | ....... | ...",
-            ".+-----+ . +-----+.",
-            ".| 003 |---| 001 |.",
-            ".+-----+ . +-----+.",
-            "... | .............",
-            ".+-----+ ..........",
-            ".| 002 | ..........",
-            ".+-----+ ..........",
-            "..................."
-        ];
-        */
        return [
         "..................................",
         ". +-----+ 126.5km +-----+ ........",
@@ -55,31 +38,5 @@ export default class GraphFactory {
         ". +-----+ ........................",
         ".................................."
        ];
-   }
-
-   private static drawVertex(id : number) : string [] {
-       return [
-           "+-----+",
-           `| ${this.drawInteger(id)} |`,
-           "+-----+",
-       ];
-   }
-   private static drawHEdge(metric1: number, metric2: number) : string [] {
-        return [
-            ` ${this.drawReal(metric1)}${GraphFactory.Metric1unit} `,
-            "<------->",
-            ` ${this.drawReal(metric2)}${GraphFactory.Metric2unit} `,
-        ];
-   }
-
-   private static drawReal(value : number) : string {
-        const arr = value.toString().split(".");
-        arr[0] = arr[0].padStart(3, "0");
-        arr[1] = arr[1].substring(0,1).padEnd(1, "0");
-        return arr.join(".");
-   }
-   private static drawInteger(value : number) : string {
-    return value.toString().padStart(3, "0");
-}
-
+    };
 }
