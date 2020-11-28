6dac41b0a12092694c2dd92f4bf96ca0fda22a29
cirrus-8691 <diaz.frederic@gmail.com>
2020-11-28 15:57:29 +0100

    meilleur representation

--- src/views/GraphFactory.ts
+++ src/views/GraphFactory.ts
@@ -6,6 +6,8 @@ import { Path } from "./interfaces/Path";
  */
 export default class GraphFactory {
 
+    private static Metric1unit = "km";
+    private static Metric2unit = "co";
     /**
      * Build the path "view"
      * @param params 
@@ -15,29 +17,69 @@ export default class GraphFactory {
     public static Build(graph : ReadonlyArray<Vertex>) : string[]
     {
         // no so easy to do.
-
+        /*
         return [
             "...................",
-            ".+-----+...+-----+.",
+            ".+-----+ . +-----+.",
             ".| 000 |---| 004 |.",
-            ".+-----+...+-----+.",
-            "....|.........|....",
-            ".+-----+...+-----+.",
+            ".+-----+ . +-----+.",
+            "... | ....... | ...",
+            ".+-----+ . +-----+.",
             ".| 003 |---| 001 |.",
-            ".+-----+...+-----+.",
-            "....|..............",
-            ".+-----+...........",
-            ".| 002 |...........",
-            ".+-----+...........",
+            ".+-----+ . +-----+.",
+            "... | .............",
+            ".+-----+ ..........",
+            ".| 002 | ..........",
+            ".+-----+ ..........",
             "..................."
         ];
+        */
+       return [
+        "..................................",
+        ". +-----+ 126.5km +-----+ ........",
+        ". | 000 |---------| 004 | ........",
+        ". +-----+ 018.7co +-----+ ........",
+        "... | ............. | ............",
+        "... | 080.2km ..... | 005.3km ....",
+        "... | 014.3co ..... | 000.2co ....",
+        "... | ............. | ............",
+        ". +-----+ 030.7km +-----+ ........",
+        ". | 003 |---------| 001 | ........",
+        ". +-----+ 004.0co +-----+ ........",
+        "... | ............................",
+        "... | 030.8km ....................",
+        "... | 004.0co ....................",
+        "... | ............................",
+        ". +-----+ ........................",
+        ". | 002 | ........................",
+        ". +-----+ ........................",
+        ".................................."
+       ];
    }
 
-   private static createVertex(id : number) : string [] {
+   private static drawVertex(id : number) : string [] {
        return [
            "+-----+",
-           `| ${id.toString(2)} |`,
+           `| ${this.drawInteger(id)} |`,
            "+-----+",
        ];
    }
+   private static drawHEdge(metric1: number, metric2: number) : string [] {
+        return [
+            ` ${this.drawReal(metric1)}${GraphFactory.Metric1unit} `,
+            "<------->",
+            ` ${this.drawReal(metric2)}${GraphFactory.Metric2unit} `,
+        ];
+   }
+
+   private static drawReal(value : number) : string {
+        const arr = value.toString().split(".");
+        arr[0] = arr[0].padStart(3, "0");
+        arr[1] = arr[1].substring(0,1).padEnd(1, "0");
+        return arr.join(".");
+   }
+   private static drawInteger(value : number) : string {
+    return value.toString().padStart(3, "0");
+}
+
 }
