32fc7f726a4fb3b56499de52ab518cba71c76c8c
cirrus-8691 <diaz.frederic@gmail.com>
2020-11-29 12:35:32 +0100

    pzth ok

--- src/views/CanvasGraphFactory.ts
+++ src/views/CanvasGraphFactory.ts
@@ -1,12 +1,10 @@
+import { ByEcoPath, ByFastestPath } from "../domain/Graph";
 import { Vertex } from "../domain/interfaces/Vertex";
 
 /**
  * To build path as expected by ihm
  */
 export default class CanvasGraphFactory {
-
-    private static Metric1unit = "km";
-    private static Metric2unit = "co2";
     /**
      * Build the path "view"
      * @param graph
@@ -22,11 +20,15 @@ export default class CanvasGraphFactory {
                 "1, 0",
             ],
             edges : [
-                "0, 0, 1, 0",
-                "1, 0, 1, 1",
-                "0, 1, 1, 1",
-                "0, 0, 0, 1",
-                "0, 1, 0, 2",
+                `0, 0, 1, 0, 125.6, 18.7`,
+                `1, 0, 1, 1,  5.3, 0.2`,
+                `0, 1, 1, 1, 30.7, 4.0`,
+                `0, 0, 0, 1, 80.2, 4.0`,
+                `0, 1, 0, 2, 30.8, 4.0`
+            ],
+            metrics : [
+                ByFastestPath,
+                ByEcoPath
             ]
         };
     };
